8f26aa416c5a98b6594ef64ee9ea52c7220f92b9
Asly Cantillo <acantill37@cuc.edu.co>
2023-09-20 17:42:18 -0500

    carpeta gif

---
+++ src/app/gifs/resultados/resultados.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'app-resultados',
+  templateUrl: './resultados.component.html',
+  styles:[
+
+  ]
+
+})
+export class ResultadosComponent {
+//acceso de un html
+      get resultados() {
+      return this.gifsService.resultados;
+      }
+
+  constructor(private gifsService : GifsService){}
+
+}
---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private apiKey : string = 'yPMeYoGoFovrJh57yl44SuIUj3zL8lsq';
+  private _historial : string []=[];
+//cambiar any por su tipo
+  public resultados: any[]=[];
+
+  get historial(){
+
+    return [...this._historial];
+  }
+
+      constructor(private http:HttpClient){}
+
+  buscarGifs( query: string){
+
+    query= query.trim().toLocaleLowerCase();
+
+  if ( !this._historial.includes(query)){
+  this._historial.unshift(query);
+  this._historial = this._historial.splice(0,10);
+}
+
+      this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=yPMeYoGoFovrJh57yl44SuIUj3zL8lsq&q=${query}&limit=10`)
+          .subscribe( (resp:any) =>{
+            console.log(resp.data);
+            this.resultados=resp.data;
+          });
+
+  }
+
+}
